1bdfba43f439af370b6fae030da11b34a1f51f56
BrianBarakaInfosecProjects <barakatechwrites@gmail.com>
2026-01-20 10:42:30 +0000

    fix seed

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -12,7 +12,7 @@ echo "PostgreSQL is up - applying migrations"
 npx prisma migrate deploy
 
 echo "Seeding database"
-npx ts-node prisma/seed.ts
+ts-node --transpile-only prisma/seed.ts
 
 echo "Starting server"
 node dist/server.js
