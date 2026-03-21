a7a88e581c283d6bbd430232baf2fb0ea996f3d5
BrianBarakaInfosecProjects <barakatechwrites@gmail.com>
2026-03-21 03:10:27 +0300

    Fix syntax error in admin vehicles page

--- frontend/app/admin/vehicles/page.tsx
+++ frontend/app/admin/vehicles/page.tsx
@@ -754,7 +754,6 @@ export default function VehiclesPage() {
                       quality={80}
                     />
                   </div>
-                  />
                 </div>
               )}
             </div>
